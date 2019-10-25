17b303b255ae0e952b3f61d4c07966e5357db61e
Will C <will@quid.works>
2019-10-25 09:53:52 -0400

    FB phone screen working answer.

--- facebook-is-ordered.js
+++ facebook-is-ordered.js
@@ -2,6 +2,7 @@ const assert = require('assert')
 
 function isSorted(words, order) {
   for (let i = 1; i < words.length; i++) {
+    console.log('compare', words[i - 1], words[i])
     if (!isOrdered(words[i - 1], words[i], order)) {
       return false
     }
@@ -15,18 +16,25 @@ function isOrdered(left, right, order) {
   let j = 0
 
   while (i < left.length || j < right.length) {
+    console.log(i, j, left.charAt(i), right.charAt(j))
     if (left.charAt(i) === right.charAt(j)) {
       i++
       j++
 
       continue
     }
+    if (order.indexOf(left.charAt(i)) < order.indexOf(right.charAt(j))) {
+      return true
+    }
 
     if (order.indexOf(left.charAt(i)) > order.indexOf(right.charAt(j))) {
       return false
-    } else if (left.charAt(i) !== undefined && right.charAt(j) === undefined) {
+    } else if (left.charAt(i) !== '' && right.charAt(j) === '') {
       return false
     }
+
+    i++
+    j++
   }
 
   return true
