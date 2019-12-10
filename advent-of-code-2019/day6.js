9bf3e9a7851f41ce77467ce8cfda5ac664783459
Will C <will@quid.works>
2019-12-10 11:36:56 -0500

    Advent of Code Day 6 part 1

--- advent-of-code-2019/day6.js
+++ advent-of-code-2019/day6.js
@@ -15,10 +15,54 @@ function readLines(input) {
   });
 }
 
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.children = [];
+  }
+}
+
 async function main(filename) {
   const input = fs.createReadStream(filename);
   const orbits = await readLines(input);
-  console.log(orbits);
+
+  const nodeIndex = {};
+  function findRoot(nodeIndex) {
+    return nodeIndex["COM"];
+  }
+
+  for (let orbit of orbits) {
+    const [center, orbitting] = orbit.split(")");
+
+    if (!nodeIndex[center]) {
+      nodeIndex[center] = new Node(center);
+    }
+
+    if (!nodeIndex[orbitting]) {
+      nodeIndex[orbitting] = new Node(orbitting);
+    }
+
+    nodeIndex[center].children.push(nodeIndex[orbitting]);
+  }
+
+  const root = findRoot(nodeIndex);
+  const stack = [[root, 0]];
+  let orbitLength = 0;
+  let currentNode;
+  let currentLength;
+  while (stack.length) {
+    [currentNode, currentLength] = stack.pop();
+
+    orbitLength += currentLength;
+
+    for (let child of currentNode.children) {
+      stack.push([child, currentLength + 1]);
+    }
+  }
+
+  return orbitLength;
 }
 
-main("day6.txt");
+main("day6.txt").then(result => {
+  console.log(result);
+});
