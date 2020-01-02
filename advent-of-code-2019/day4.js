0da07c49beb1b7ab196cff8827881eecc890ea94
Will C <will@quid.works>
2020-01-02 05:19:45 -0500

    advent of code day 4 part 2

--- advent-of-code-2019/day4.js
+++ advent-of-code-2019/day4.js
@@ -1,25 +1,57 @@
 // const input = '278384-824795'
 
+const assert = require("assert");
+
 let count = 0;
 let string;
 
-for (let i = 278384; i <= 824795; i++) {
-  string = String(i);
-  let valid = true;
+function hasDupe(str) {
   let hasDup = false;
-  // console.log(i);
-  for (let j = 1; j < string.length; j++) {
-    if (string.charAt(j) === string.charAt(j - 1)) {
-      hasDup = true;
+  let j = 0;
+  while (j < str.length) {
+    let count = 0;
+    let num = str.charAt(j);
+    while (str.charAt(j) === num) {
+      j++;
+      count++;
     }
-    if (string.charAt(j) < string.charAt(j - 1)) {
+    // if (count > 1 && count % 2 !== 0) return false;
+    if (count === 2) hasDup = true;
+  }
+
+  return hasDup;
+}
+
+assert.equal(hasDupe("123444"), false);
+assert.equal(hasDupe("124444"), false);
+assert.equal(hasDupe("113334"), true);
+assert.equal(hasDupe("111334"), true);
+assert.equal(hasDupe("113345"), true);
+assert.equal(hasDupe("111122"), true);
+assert.equal(hasDupe("112233"), true);
+assert.equal(hasDupe("123445"), true);
+assert.equal(hasDupe("123456"), false);
+
+function isAsc(str) {
+  for (let j = 1; j < str.length; j++) {
+    if (str.charAt(j) < str.charAt(j - 1)) {
       // console.log("next char less then prev");
-      valid = false;
-      break;
+      return false;
     }
   }
 
-  if (!valid) continue;
+  return true;
+}
+
+assert.equal(isAsc("123456"), true);
+assert.equal(isAsc("213456"), false);
+
+for (let i = 278384; i <= 824795; i++) {
+  string = String(i);
+  let isAscending = isAsc(string);
+  let hasDup = hasDupe(string);
+
+  if (!isAscending) continue;
 
   if (!hasDup) continue;
 
